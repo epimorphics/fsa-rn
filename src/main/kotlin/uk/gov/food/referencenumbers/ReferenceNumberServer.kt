c2e3ca547e47b8b2ee25e3da3466f50a1b872002
Max Prettyjohns <heshoots9999@gmail.com>
2018-04-26 11:02:36 +0100

    Add cache control headers

--- src/main/kotlin/uk/gov/food/referencenumbers/ReferenceNumberServer.kt
+++ src/main/kotlin/uk/gov/food/referencenumbers/ReferenceNumberServer.kt
@@ -1,18 +1,28 @@
 package uk.gov.food.referencenumbers
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.food.rn.*
+import com.fasterxml.jackson.annotation.*
 import java.time.ZonedDateTime
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+class FSARN(@JsonProperty("fsa-rn") val rn: String)
 
 @RestController
 class GreetingController(val config: ReferenceNumbersConfig) {
     @GetMapping("/fsa-rn/{authority}/{type}")
-    fun get(@PathVariable authority: Int, @PathVariable type: Int) : (ResponseEntity<String>) {
+    fun get(@PathVariable authority: Int, @PathVariable type: Int) : (ResponseEntity<FSARN>) {
         var x = RNFactory.getFactory(Authority(authority), Instance(config.instance), Type(type))
-        return ResponseEntity.ok(x.generateReferenceNumber().toString())
+        var rn = FSARN(x.generateReferenceNumber().toString())
+        var responseHeaders = HttpHeaders()
+        responseHeaders.set("Cache-Control", "no-cache,no-store,must-revalidate")
+        responseHeaders.set("pragma", "no-cache")
+        responseHeaders.set("Expires", "0")
+        return ResponseEntity(rn, responseHeaders, HttpStatus.OK)
     }
 }
 
