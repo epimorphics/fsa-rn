1ea37dff24339796403cfb2b03aa12b90df4c1b9
Max Prettyjohns <heshoots9999@gmail.com>
2018-11-26 14:53:56 +0000

    add tests for new generator code and update pebble

--- src/test/kotlin/uk/gov/food/referencenumbers/GenerateControllerTests.kt
+++ src/test/kotlin/uk/gov/food/referencenumbers/GenerateControllerTests.kt
@@ -1,12 +1,13 @@
 package uk.gov.food.referencenumbers
 
-import junit.framework.TestCase.assertFalse
-import junit.framework.TestCase.assertTrue
+import junit.framework.TestCase.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import uk.gov.food.rn.Authority
+import uk.gov.food.rn.RNException
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -27,5 +28,44 @@ class GenerateControllerTests {
         assertFalse(controller.ContainsBadWord("CG1B6L-9DBNND-M14W5G"))
         assertFalse(controller.ContainsBadWord("JA55M9-G4WEXE-V2204L"))
         assertFalse(controller.ContainsBadWord("C5GX1F-73WE46-Q52D89"))
+    }
+
+    @Test
+    fun ShouldGenerateReferenceNumbers() {
+        assertNotNull(controller.GetReferenceNumber(Authority(1000), "100"))
+        assertNotNull(controller.GetReferenceNumber(Authority(2000), "200"))
+        assertNotNull(controller.GetReferenceNumber(Authority(3000), "300"))
+    }
+
+    @Test
+    fun ShouldThrowRNExceptionOnInvalidAuthority() {
+        try {
+            controller.GetReferenceNumber(Authority(100), "001")
+            fail("Expected reference number generation to fail")
+        } catch (e : RNException) {
+            assertEquals("Illegal identifier for authority: 100 is not in the range 1000 : 9999", e.message)
+        }
+        try {
+            controller.GetReferenceNumber(Authority(10000), "100")
+            fail("Expected reference number generation to fail")
+        } catch (e : RNException) {
+            assertEquals("Illegal identifier for authority: 10000 is not in the range 1000 : 9999", e.message)
+        }
+    }
+
+    @Test
+    fun ShouldThrowInvalidParameterExceptionOnInvalidType() {
+        try {
+            controller.GetReferenceNumber(Authority(1001), "1001")
+            fail("Expected reference number generation to fail")
+        } catch (e : InvalidParameterException) {
+            assertEquals("Type parameter invalid length, example: 105", e.message)
+        }
+        try {
+            controller.GetReferenceNumber(Authority(9999), "00")
+            fail("Expected reference number generation to fail")
+        } catch (e : InvalidParameterException) {
+            assertEquals("Type parameter invalid length, example: 105", e.message)
+        }
     }
 }
